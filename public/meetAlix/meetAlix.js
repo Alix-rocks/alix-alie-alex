d1d32777fe52aca278f8929439ccd8872d59f3e0
Alix <alexblade.23.49@gmail.com>
2025-05-24 11:21:30 -0400

    update meetAlix

--- public/meetAlix/meetAlix.js
+++ public/meetAlix/meetAlix.js
@@ -1,6 +1,6 @@
 import { app, analytics, db, auth, provider, getFirestore, collection, getDocs, getDoc, query, where, addDoc, deleteDoc, doc, setDoc, updateDoc, deleteField, writeBatch, Timestamp, getAuth, GoogleAuthProvider, signOut, signInWithRedirect, getRedirectResult, onAuthStateChanged } from "../../myFirebase.js";
 
-let unknownStartDate = "2025-05-11"; //The day the "Not sure yet" section starts
+let unknownStartDate = "2025-06-16"; //The day the "Not sure yet" section starts
 
 
 let myEmail = "alexblade.23.49@gmail.com";
